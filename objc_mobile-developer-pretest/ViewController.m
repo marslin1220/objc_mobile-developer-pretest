87964ca8d0ca69aef7c7e3f2a94b8051d3e635e4
Cheng Lung Lin <marslin@gmail.com>
2018-04-12 09:33:46 +0800

    Fetch drama list information with AFNetworking

--- objc_mobile-developer-pretest/ViewController.m
+++ objc_mobile-developer-pretest/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+#import <AFNetworking.h>
+
+static NSString *const dataSourceURL = @"http://www.mocky.io/v2/5a97c59c30000047005c1ed2";
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *dramaList;
 
 @end
 
@@ -16,14 +22,74 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self fetchDramaList];
 }
 
+- (void)fetchDramaList {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+
+    NSURL *URL = [NSURL URLWithString:dataSourceURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request
+        uploadProgress:^(NSProgress *_Nonnull uploadProgress) {
+
+        }
+        downloadProgress:^(NSProgress *_Nonnull downloadProgress) {
+
+        }
+        completionHandler:^(NSURLResponse *_Nonnull response, id _Nullable responseObject, NSError *_Nullable error) {
+          if (error) {
+              NSLog(@"Error: %@", error);
+          } else {
+              NSLog(@"%@ %@", response, responseObject);
+          }
+        }];
+
+    [dataTask resume];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.dramaList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"dramaInfo" forIndexPath:indexPath];
+
+    if (NULL == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"dramaInfo"];
+    }
+
+    [self configureCell:cell forRowAtIndexPath:indexPath];
+
+    return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *dramaInfo = self.dramaList[indexPath.row];
+
+    cell.textLabel.text = dramaInfo[@"name"];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"rating: %@\ncreated at: %@", dramaInfo[@"rating"], dramaInfo[@"created_at"]];
+    cell.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:dramaInfo[@"thumb"]]];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
 
 @end
